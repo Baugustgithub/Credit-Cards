5885d98bcdb0bd8271a0be122b4a46ab01cd7917
Baugustgithub <40409162+Baugustgithub@users.noreply.github.com>
2025-04-28 01:01:58 -0400

    Update cards_db.py

--- cards_db.py
+++ cards_db.py
@@ -6,113 +6,104 @@ cards_data = [
         "base_rate": 1.0,
         "static_bonuses": {"groceries": 3, "gas": 3, "transit": 3, "restaurants": 3},
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "Amex Blue Cash Everyday",
         "base_rate": 1.0,
         "static_bonuses": {"groceries": 3, "online retail": 3, "gas": 3},
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "Chase Prime Visa",
         "base_rate": 1.0,
         "static_bonuses": {"amazon": 5, "whole foods": 5, "gas": 2, "restaurants": 2, "drugstores": 2},
         "rotating_bonuses": [],
+        "wallet_card": False
     },
     {
         "name": "Chase Freedom Flex",
         "base_rate": 1.0,
         "static_bonuses": {"dining": 3, "drugstores": 3},
         "rotating_bonuses": [],
-    },
-    {
-        "name": "PNC Cash Unlimited",
-        "base_rate": 2.0,
-        "static_bonuses": {},
-        "rotating_bonuses": [],
-    },
-    {
-        "name": "PNC Cash Rewards Visa",
-        "base_rate": 1.0,
-        "static_bonuses": {"gas": 4, "dining": 3, "groceries": 2},
-        "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "US Bank Cash+",
         "base_rate": 1.0,
         "static_bonuses": {"home utilities": 5, "streaming services": 5},
         "rotating_bonuses": [],
+        "wallet_card": False
     },
     {
         "name": "US Bank Altitude Go Visa",
         "base_rate": 1.0,
         "static_bonuses": {"dining": 4, "gas": 2, "streaming": 2, "groceries": 2},
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "Citi Double Cash",
         "base_rate": 2.0,
         "static_bonuses": {},
         "rotating_bonuses": [],
+        "wallet_card": False
     },
     {
         "name": "Citi Custom Cash",
         "base_rate": 1.0,
-        "static_bonuses": {"dining": 5},  # Only apply to Dining
+        "static_bonuses": {"dining": 5},  # Only use for Dining
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "Nusenda Platinum Cash Rewards",
         "base_rate": 1.0,
         "static_bonuses": {},
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "FNBO Ducks Unlimited",
         "base_rate": 1.0,
         "static_bonuses": {"gas": 5, "sporting goods": 5},
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "FNBO Evergreen",
         "base_rate": 2.0,
         "static_bonuses": {},
         "rotating_bonuses": [],
+        "wallet_card": False
     },
     {
         "name": "Discover It",
         "base_rate": 1.0,
         "static_bonuses": {},
         "rotating_bonuses": [],
-    },
-    {
-        "name": "Barclays View Mastercard",
-        "base_rate": 1.0,
-        "static_bonuses": {"dining": 3, "takeout": 3, "grocery": 3, "streaming": 3, "utilities": 2},
-        "rotating_bonuses": [],
-    },
-    {
-        "name": "Capital One Quicksilver",
-        "base_rate": 1.5,
-        "static_bonuses": {},
-        "rotating_bonuses": [],
-    },
-    {
-        "name": "Wells Fargo Autograph",
-        "base_rate": 1.0,
-        "static_bonuses": {"dining": 3, "travel": 3, "gas": 3, "transit": 3, "streaming": 3, "phone": 3},
-        "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
         "name": "Bread AAA (Comenity)",
         "base_rate": 1.0,
         "static_bonuses": {"groceries": 5, "wholesale clubs": 3, "gas": 2, "restaurants": 2, "travel": 2},
         "rotating_bonuses": [],
+        "wallet_card": True
     },
     {
-        "name": "Elan Max Cash Preferred",
+        "name": "Wells Fargo Autograph",
         "base_rate": 1.0,
-        "static_bonuses": {"cell phone": 5, "electric stores": 5},
+        "static_bonuses": {"dining": 3, "travel": 3, "gas": 3, "transit": 3, "streaming": 3, "phone": 3},
         "rotating_bonuses": [],
+        "wallet_card": True
+    },
+    {
+        "name": "Fidelity Rewards Visa",
+        "base_rate": 2.0,
+        "static_bonuses": {},
+        "rotating_bonuses": [],
+        "wallet_card": True
     }
 ]
